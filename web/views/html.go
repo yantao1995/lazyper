18913e2e2e00633e16d1f0176ef25c1280002abf
Tany <1173904005@qq.com>
2022-12-20 17:51:00 +0800

    first commit

--- web/views/html.go
+++ web/views/html.go
@@ -11,13 +11,13 @@ func init() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("%#v", addrs)
 	for _, address := range addrs {
 		// 检查ip地址判断是否回环地址
 		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
+			if ipnet.IP.To4() != nil && ipnet.IP.DefaultMask().String() == "ff000000" {
 				fmt.Println(ipnet.IP.String())
 			}
+
 		}
 	}
 
